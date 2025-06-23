1efa8ea63b58b96a8608bf88e5845c58e3065e4d
신동현 <donghyeun02@gmail.com>
2025-06-23 16:07:10 +0900

    feat: MDX 설정 및 글 폴더 초기 구성

---
+++ frontend/src/app/mdx/logic-gates/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import LogicGatesContent from './content.mdx';
+
+export default function LogicGatesPage() {
+  return (
+    <main className="prose mx-auto p-8">
+      <LogicGatesContent />
+    </main>
+  );
+}
