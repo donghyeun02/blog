a043a837a894cf305faf4a8a97ee955037806c61
신동현 <donghyeun02@gmail.com>
2025-06-25 17:48:50 +0900

    chore: 중복 폴더/파일 삭제

--- tailwind.config 2.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{ts,tsx,js,jsx,mdx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-};
