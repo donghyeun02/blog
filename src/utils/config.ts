e74c5a1d9850d5c47fe28ac38c11e5bd4f97d532
신동현 <donghyeun02@gmail.com>
2025-07-21 18:55:09 +0900

    chore: Pinata 웹 업로드 방식으로 전환

--- src/utils/config.ts
+++
@@ -1,8 +0,0 @@
-import { PinataSDK } from 'pinata';
-
-export function getPinata() {
-  return new PinataSDK({
-    pinataJwt: process.env.PINATA_JWT!,
-    pinataGateway: process.env.NEXT_PUBLIC_GATEWAY_URL!,
-  });
-}
